d5806ab17a2e2d37c731d994123147cf6434b695
Blake Dworaczyk <bdd4329@tamu.edu>
2023-02-28 21:35:46 -0600

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_profile_name" {
+  type        = string
+  description = "The AWS profile name used to authenticate"
+}
+
+variable "dynamo_db_table_name" {
+  type = string
+  description = "Name of the DynamoDB table used for state locking"
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket used to store state"
+}
